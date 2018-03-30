9984ad5425a8191634824569e0a26bca8390cf8f
hilmayunidamj1 <hilmayunidamj@gmail.com>
2018-03-30 04:46:35 -0700

    Membuat Attribute dan Bahavior

--- src/praktek02/Kerudung.java
+++ src/praktek02/Kerudung.java
@@ -1,4 +1,13 @@
 package praktek02;
 public class Kerudung {
+    Integer Harga;
+    Integer Jumlah;
+    
+    void cetakInfo(){
+        System.out.println("===============");
+        System.out.println("Harga = "+Harga);
+        System.out.println("Jumlah ="+Jumlah);
+        System.out.println("===============");
+    }
     
 }
