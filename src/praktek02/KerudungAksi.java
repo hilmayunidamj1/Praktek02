3a76ee8c1173588e889bc7343d352cf4b2cd3899
hilmayunidamj1 <hilmayunidamj@gmail.com>
2018-03-30 05:39:33 -0700

    Constructor dengan parameter

--- src/praktek02/KerudungAksi.java
+++ src/praktek02/KerudungAksi.java
@@ -9,6 +9,12 @@ public class KerudungAksi {
         rl.cetakInfo();
         System.out.println("Total Kerudung = "+rl.hitungTotal());
         rl.cetakTotal();
+        
+        Kerudung r2 = new Kerudung();
+        r2.cetakInfo();
+        
+        Kerudung r3 = new Kerudung(10,10);
+        r3.cetakInfo();
     }
     
 }
