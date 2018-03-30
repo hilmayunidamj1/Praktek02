4ab0b7dc294746768898a29ed145b2abf22b176e
hilmayunidamj1 <hilmayunidamj@gmail.com>
2018-03-30 04:59:50 -0700

    Mengisi Nilai attribute

--- src/praktek02/KerudungAksi.java
+++ src/praktek02/KerudungAksi.java
@@ -3,6 +3,8 @@ package praktek02;
 public class KerudungAksi {
     public static void main(String[] args) {
         Kerudung rl = new Kerudung();
+        rl.Harga = 50000;
+        rl.Jumlah = 10;
     }
     
 }
